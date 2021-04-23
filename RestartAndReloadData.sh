b37b6dd07e3bc15c008618dc5758ff1605b2a111
Dinis Trigo <dinis.f.trigo@inesctec.pt>
2021-04-23 16:27:34 +0100

    added a script to clean-up turnkey and restart it

---
+++ RestartAndReloadData.sh
@@ -0,0 +1,14 @@
+# stop turnkey
+scripts/stop_turnkey.sh 
+
+# delete database folder contents
+sudo rm -Rf .mongodb_data/*
+
+sudo docker system prune -a
+
+# start turnkey
+scripts/start_turnkey.sh
+
+scripts/load_metadata.sh ireceptor test_data/PRJNA330606_Wang_1_sample_metadata.csv
+
+curl --data "{}" "http://localhost/airr/v1/repertoire"
